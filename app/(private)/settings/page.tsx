c0a48c28c33e9134bc111590d3b010598da3218c
Gaurav <burandegaurav8899@gmail.com>
2024-09-25 14:26:43 +0530

    feat: now billing is properly working!

--- app/(private)/settings/page.tsx
+++ app/(private)/settings/page.tsx
@@ -1,42 +1,34 @@
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
-  AlertTriangle,
-  Calendar,
-  CheckCircle,
-  CircleUser,
-  Clock,
-  CreditCard,
-  HardDrive,
-  ImageIcon,
-  Mail,
-} from "lucide-react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { CircleUser, HardDrive, ImageIcon, Mail } from "lucide-react";
 import UsageCard from "@/components/studio/blocks/usage";
-import { formatDate } from "@/helpers/utils";
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
 import { DangerZone } from "@/components/blocks/dangerZone";
 import { getBilling, getUser } from "@/lib/db";
 import { PlainUser } from "@/helpers/types";
-import { useRouter } from "next/navigation";
+import { BillingSection } from "@/components/blocks/billing";
 
 interface UserProps {
   user: PlainUser;
 }
 export default async function Settings() {
   const user = await getUser();
+  let billing: any = await getBilling();
+  if (!billing) {
+    billing = {
+      plan: "Free",
+      status: "unpaid",
+      currentPeriodStart: undefined,
+      currentPeriodEnd: undefined,
+      cancelAtPeriodEnd: new Date(),
+      endedAt: undefined,
+    };
+  }
   if (!user) {
     return null;
   }
   return (
     <main className="min-h-screen space-y-12 p-10 w-full max-w-3xl mx-auto">
       <AccountOverview user={user} />
-      <BillingSection user={user} />
+      <BillingSection billing={billing} user={user} />
       <DangerZone user={user} />
     </main>
   );
@@ -74,154 +66,6 @@ const AccountOverview = async ({ user }: UserProps) => {
           />
         </div>
       </CardContent>
-    </Card>
-  );
-};
-
-const getStatusColor = (status: string): string => {
-  switch (status) {
-    case "active":
-      return "bg-green-500";
-    case "canceled":
-    case "past_due":
-    case "unpaid":
-      return "bg-red-500";
-    case "incomplete":
-      return "bg-yellow-500";
-    default:
-      return "bg-gray-500";
-  }
-};
-
-const BillingSection = async ({ user }: UserProps) => {
-  let billing: any = await getBilling();
-  if (!billing) {
-    billing = {
-      plan: "Free",
-      status: "unpaid",
-      currentPeriodStart: undefined,
-      currentPeriodEnd: undefined,
-      cancelAtPeriodEnd: false,
-      endedAt: undefined,
-    };
-  }
-
-  const router = useRouter();
-
-  const handleBilling = async () => {
-    try {
-      if (user.hasAccess) {
-        const response = await fetch("/api/stripe/create-portal", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            returnUrl: window.location.origin,
-          }),
-        });
-
-        const data = await response.json();
-        if (data.url) {
-          window.location.href = data.url;
-        }
-      } else {
-        router.push("/studio#upgrade");
-      }
-    } catch (error) {}
-  };
-
-  return (
-    <Card id="billing">
-      <CardHeader className="flex justify-between items-start">
-        <CardTitle className="flex text-accent mb-2 items-center space-x-2">
-          <CreditCard className="h-6 w-6" />
-          <span>Billing</span>
-        </CardTitle>
-        <Badge
-          variant="secondary"
-          className="bg-primary text-primary-foreground"
-        >
-          {billing?.plan} Plan
-        </Badge>
-      </CardHeader>
-      <CardContent className="space-y-6">
-        <div className="flex justify-between items-center">
-          <span className="text-md font-semibold text-foreground">
-            Subscription Status
-          </span>
-          <Badge
-            className={`${getStatusColor(
-              billing?.status || "unpaid"
-            )} text-primary-foreground capitalize`}
-          >
-            {billing?.status}
-          </Badge>
-        </div>
-
-        <div className="space-y-4">
-          <div className="flex items-center space-x-2 text-muted-foreground">
-            <Calendar className="text-primary" />
-            <span className="font-medium">Current Billing Period:</span>
-            {billing?.currentPeriodStart ? (
-              <span>
-                {formatDate(billing?.currentPeriodStart)} -{" "}
-                {formatDate(billing?.currentPeriodEnd)}
-              </span>
-            ) : (
-              <span>User hasn&apos;t purchased any plan yet!</span>
-            )}
-          </div>
-
-          {billing?.endedAt && (
-            <div className="flex items-center space-x-2 text-muted-foreground">
-              <Clock className="text-primary" />
-              <span className="font-medium">Subscription Ended:</span>
-              <span>{formatDate(billing.endedAt)}</span>
-            </div>
-          )}
-        </div>
-
-        {billing?.currentPeriodStart && (
-          <div>
-            {billing?.cancelAtPeriodEnd ? (
-              <div className="bg-yellow-100 text-yellow-800 p-4 rounded-md flex items-center space-x-2">
-                <AlertTriangle className="text-yellow-800" />
-                <span>
-                  Your subscription will be canceled at the end of the current
-                  billing period.
-                </span>
-              </div>
-            ) : billing?.endedAt ? (
-              <div className="bg-red-100 text-red-800 p-4 rounded-md flex items-center space-x-2">
-                <AlertTriangle className="text-red-800" />
-                <span>
-                  Your subscription has ended. Renew to continue using premium
-                  features.
-                </span>
-              </div>
-            ) : (
-              <div className="bg-emerald-100 text-emerald-800 p-4 rounded-md flex items-center space-x-2">
-                <CheckCircle className="text-emerald-800" />
-                <span>
-                  Your subscription will automatically renew at the end of the
-                  billing period.
-                </span>
-              </div>
-            )}
-          </div>
-        )}
-      </CardContent>
-      <CardFooter>
-        {/* todo: add link to manage subscription */}
-        <Button onClick={handleBilling} className="w-full">
-          {billing?.endedAt
-            ? "Renew Subscription"
-            : billing?.cancelAtPeriodEnd
-            ? "Resume Subscription"
-            : "Manage Subscription"}
-        </Button>
-      </CardFooter>
     </Card>
   );
 };
