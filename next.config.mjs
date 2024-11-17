4fd6f5bb0e936b5a5d68b97d98875f3cd17e9838
Gaurav <burandegaurav8899@gmail.com>
2024-11-17 22:16:59 +0530

    feat: security headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
     images: {
+        // todo: be secure here, it's fine for now.
         remotePatterns: [
             {
                 protocol: 'https',
@@ -8,6 +10,29 @@ const nextConfig = {
             },
         ],
     },
+    async headers() {
+        return {
+            source: "/(.*)",
+            headers: [
+                {
+                    key: "Strict-Transport-Security",
+                    value: "max-age=31536000; includeSubDomains; preload",
+                },
+                {
+                    key: "X-Frame-Options",
+                    value: "DENY",
+                },
+                {
+                    key: "X-Content-Type-Options",
+                    value: "nosniff",
+                },
+                {
+                    key: "Referrer-Policy",
+                    value: "strict-origin-when-cross-origin",
+                },
+            ],
+        };
+    }
 };
 
 export default nextConfig;
